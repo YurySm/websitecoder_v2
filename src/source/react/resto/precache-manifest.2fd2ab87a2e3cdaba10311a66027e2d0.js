4a5976a776238d2ae5f9f4cee7c0cedbf0047dcd
Yury <yurysmolka@gmail.com>
2021-03-25 14:42:42 +0300

    add desccr rect

---
+++ src/source/react/resto/precache-manifest.2fd2ab87a2e3cdaba10311a66027e2d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3156d84fb4ebee4171daad79729993f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "642dc6932abbfb3669a1",
+    "url": "/static/css/main.7574d9f1.chunk.css"
+  },
+  {
+    "revision": "c176b4509c5b9e6f09f9",
+    "url": "/static/js/2.6b9c0686.chunk.js"
+  },
+  {
+    "revision": "642dc6932abbfb3669a1",
+    "url": "/static/js/main.e42e4668.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
